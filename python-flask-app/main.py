a85cbc7c698cb7153fe57dcc5731225b5b75ed51
Rodrigo Ortega <91147909+RodrigoOrtega1@users.noreply.github.com>
2025-08-27 18:30:05 -0600

    refactor: cambio

--- python-flask-app/main.py
+++ python-flask-app/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, World! This is a simple Python web app."
+    return "Adios mundo cruel"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=3000)
